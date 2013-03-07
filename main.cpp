ffe91c9d3c81188109e47a23c1833ba046b471a5
Nomyx <r.noemie25@orange.fr>
2013-03-07 13:02:30 +0100

    Add the score of the player and add the conversion for UTF8

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@
 
 int main(int argc, char *argv[])
 {
+
+#if QT_VERSION < 0x050000
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#endif
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
